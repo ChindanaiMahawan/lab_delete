1f8b92607aced7051d84526c80f456113a122c2a
ChindanaiMahawan <dewar.chindanai@gmail.com>
2026-08-21 18:43:55 +0700

    add text

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -4,7 +4,7 @@
       <img src="/src/assets/CJ.jpg" />
     </div>
     <div class="q-gutter-y-md column" style="max-width: 300px">
-      <q-input color="purple-12" v-model="text" label="(ชื่อ)">
+      <q-input color="purple-12" v-model="text" label="(ชื่ออะไรวะ)">
         <template v-slot:prepend>
           <q-icon name="event" />
         </template>
